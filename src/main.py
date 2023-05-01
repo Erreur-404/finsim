7634201a4b09996f9f4fbafc39ac9d0dc2cc2cce
Erreur-404 <etienne-2.perron@polymtl.ca>
2023-05-01 17:00:57 -0400

    Add consideration of command line arguments in simulation

--- src/main.py
+++ src/main.py
@@ -3,40 +3,53 @@ from argparse import ArgumentParser
 from wallet import Wallet
 from strategy import Strategy
 
+
 """
 Sets the argument parser to interface with the CLI
 """
 def set_args():
-    arg_parser = ArgumentParser(
-    prog = 'finsim',
-    usage = '%(prog) TODO')
+    arg_parser = ArgumentParser(prog='finsim',
+                                usage='finsim TICKER [options]',
+                                exit_on_error = True)
     
-    arg_parser.add_argument('--ticker', '-t',
-                            help='The ticker on which the simulation will be executed',
-                            required=True)
+    arg_parser.add_argument('ticker',
+                            help='The ticker on which the simulation will be executed')
 
     arg_parser.add_argument('--period', '-p',
                             help='The period of the simulation. Possible values: 1d, 5d, 1mo, 3mo, 6mo, 1y, 2y, 5y, 10y, ytd, max',
-                            default='1mo',
-                            required=False)
+                            default='1mo')
+
+    arg_parser.add_argument('--interval', '-i',
+                            help='The interval between two timestamps. Possible values: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 1d, 5d, 1wk, 1mo, 3mo',
+                            default='1d')
 
     # TODO : Complete
+    arguments = arg_parser.parse_args()
+    arguments.ticker = arguments.ticker.upper()
+    return arguments
+
 
 """
 Perform the simulation
+\param      cli_args : The command line arguments that were supplied by the user
 """
-def simulate():
+def simulate(cli_args):
     initial_cash = 100000
     wallet = Wallet(initial_cash)
-    print('[*] Downloading MSFT data...') # TODO : Replace MSFT
-    ticker_data = yf.download(tickers="MSFT", period="1d", interval="1m", prepost=False, repair=True) # TODO : Adjust according to the command line parameters
+    print(f'[*] Downloading {cli_args.ticker} data...')
+    # TODO : Handle error in download (incorrect ticker)
+    ticker_data = yf.download(tickers=cli_args.ticker, 
+                                period=cli_args.period, 
+                                interval=cli_args.interval, 
+                                prepost=False, 
+                                repair=True)
     strategy = Strategy(ticker_data)
     print('[*] Simulating...')
     for i in range(len(ticker_data.index)):
         if strategy.should_buy(i): # TODO : Send date instead of Series
-            wallet.buy("MSFT", 100, 12) # TODO : Determine how much
+            wallet.buy(cli_args.ticker, 100, 12) # TODO : Determine how much
         elif strategy.should_sell(i):
-            wallet.sell("MSFT", 100, 12) # TODO : Determine how much
+            wallet.sell(cli_args.ticker, 100, 12) # TODO : Determine how much
     # TODO : Sell all remaining assets (or find a way to show them in the final output)
     final_cash = wallet.cash
     print('[+] Simulation done')
@@ -45,5 +58,5 @@ def simulate():
 
 
 if __name__ == '__main__':
-    set_args()
-    simulate()
+    args = set_args()
+    simulate(args)
